cb67ff2f460374a8ec3845ef18ac5df96e9da056
Maksym Bespalykh <99548337+Marsen2@users.noreply.github.com>
2022-07-21 17:44:51 +0300

    Add files via upload

---
+++ ls_2/index.js
@@ -0,0 +1,11 @@
+let inputElem = document.querySelector('.only_input')
+
+export function getInputElement() {
+    console.dir(inputElem)
+}
+getInputElement()
+let titleElem = document.querySelector('.title')
+export function getTitleElement(){
+    console.dir(titleElem)
+}
+getTitleElement()
