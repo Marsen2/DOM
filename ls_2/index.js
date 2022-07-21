8d5951caa8afb38a5ca244bbfe4f63947219d7e8
Maksym Bespalykh <99548337+Marsen2@users.noreply.github.com>
2022-07-21 20:15:06 +0300

    Update index.js

--- ls_2/index.js
+++ ls_2/index.js
@@ -1,10 +1,10 @@
 export function getTitleElement(){
-    let titleElem = document.querySelector('.title')
+    return titleElem = document.querySelector('.title')
     console.dir(titleElem)
 }
 getTitleElement()
 export function getInputElement() {
-    let inputElem = document.querySelector('input[type=text]')
+    return inputElem = document.querySelector('input[type=text]')
     console.dir(inputElem)
 }
 getInputElement()
