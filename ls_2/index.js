12fe8df6158b86d71287b87a55430988e1bae821
Maksym Bespalykh <99548337+Marsen2@users.noreply.github.com>
2022-07-21 18:27:07 +0300

    Add files via upload

--- ls_2/index.js
+++ ls_2/index.js
@@ -1,10 +1,11 @@
-export function getInputElement() {
-    let inputElem = document.querySelector('input[type=text]')
-    console.dir(inputElem)
-}
-getInputElement()
 export function getTitleElement(){
     let titleElem = document.querySelector('.title')
     console.dir(titleElem)
 }
 getTitleElement()
+export function getInputElement() {
+    let inputElem = document.querySelector('input[type=text]')
+    console.dir(inputElem)
+}
+getInputElement()
+
