d386ec808b2b5ee7fcf2c2e718a65cabe88d7d79
Maksym Bespalykh <99548337+Marsen2@users.noreply.github.com>
2022-07-22 14:28:25 +0300

    Add files via upload

---
+++ ls_4/index.js
@@ -0,0 +1,12 @@
+export function getTitle(){
+    return document.querySelector('.title').textContent
+}
+export function getDescription(){
+    return document.querySelector('.about').innerText
+}
+export function getPlans(){
+    return document.querySelector('.plans').innerHTML
+}
+export function getGoal(){
+    return document.querySelector('.goal').outerHTML
+}
