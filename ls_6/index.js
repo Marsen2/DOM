fb8f2493116f6019a018ffca894133c70dd300ad
Maksym Bespalykh <99548337+Marsen2@users.noreply.github.com>
2022-07-22 16:29:53 +0300

    Add files via upload

--- ls_6/index.js
+++ ls_6/index.js
@@ -1,4 +1,4 @@
 export function setButton(buttonText){
-    let res = document.querySelector('body')
-    return res.innerHTML = buttonText
+    return document.querySelector('body').innerHTML = buttonText
+
 }
