a48c05d2c8c7d0499072b1961191f0dc6dc8ed21
Maksym Bespalykh <99548337+Marsen2@users.noreply.github.com>
2022-07-25 17:32:07 +0300

    Add files via upload

---
+++ ls_10/index.js
@@ -0,0 +1,9 @@
+// algo
+// 1. пройтисьс по инпуту
+// 2. вставить вначало инпут
+// 3. зменить атрибут с текст на password
+
+function finishForm(){
+    let elem = document.querySelector('input').before()
+
+}
