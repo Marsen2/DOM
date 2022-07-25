96fa8d73b3e66e6444c09fc7a826527fd9f7d518
Maksym Bespalykh <99548337+Marsen2@users.noreply.github.com>
2022-07-25 17:34:25 +0300

    Add files via upload

--- ls_10/index.js
+++ ls_10/index.js
@@ -3,7 +3,8 @@
 // 2. вставить вначало инпут
 // 3. зменить атрибут с текст на password
 
-function finishForm(){
-    let elem = document.querySelector('input').before()
+export function finishForm(){
+    let elem = document.querySelector('input')
+    elem.setAttribute('password', '')
 
 }
